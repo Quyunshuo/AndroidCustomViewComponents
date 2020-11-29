5da766319ebf2fb9d06a4f0d309038ba658d6285
Quyunshuo <quyunshuo@qq.com>
2020-11-29 21:31:45 +0800

    创建base组件和绘图基础Module

--- DrawingBasis/src/main/java/com/quyunshuo/drawingbasis/MainActivity.kt
+++ DrawingBasis/src/main/java/com/quyunshuo/drawingbasis/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.quyunshuo.customview
+package com.quyunshuo.drawingbasis
 
 import androidx.viewbinding.ViewBinding
 import com.quyunshuo.base.BaseActivity
-import com.quyunshuo.customview.databinding.ActivityMainBinding
+import com.quyunshuo.drawingbasis.databinding.ActivityMainBinding
 
 /**
- * Android 自定义View组件
+ * 绘图基础
  *
  * @author Qu Yunshuo
- * @since 11/29/20 9:29 PM
+ * @since 11/29/20 9:01 PM
  */
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
---
+++ lib_base/src/main/java/com/quyunshuo/base/BaseActivity.kt
@@ -0,0 +1,31 @@
+package com.quyunshuo.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
+
+/**
+ * Activity 基类
+ *
+ * @author Qu Yunshuo
+ * @since 11/29/20 9:16 PM
+ */
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+
+    protected val mBinding: VB by lazy(mode = LazyThreadSafetyMode.NONE) {
+        val vbClass: Class<VB> =
+            (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<VB>
+        val inflate = vbClass.getDeclaredMethod("inflate", LayoutInflater::class.java)
+        inflate.invoke(null, layoutInflater) as VB
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mBinding.root)
+        mBinding.initView()
+    }
+
+    protected abstract fun ViewBinding.initView()
+}
